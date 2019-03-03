1c29d2fef3e950f0845bb71d1c040eeb0474ffda
Mod Nattasit <mod_nattasit@outlook.com>
2019-03-03 16:09:26 +0700

    Update By Mod

--- StudentCheck.DataAccess/ModelViews/ManageProfessorViewModel.cs
+++ StudentCheck.DataAccess/ModelViews/ManageProfessorViewModel.cs
@@ -2,6 +2,7 @@
 using StudentCheck.DataAccess.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,13 +16,21 @@ namespace StudentCheck.DataAccess.ModelViews
 
     public class CreateProfessorModel
     {
+        [Required]
         public string ProfessorsId { get; set; }
+        [Required]
         public string ProfessorsFirstname { get; set; }
+        [Required]
         public string ProfessorsLastname { get; set; }
+        [Required]
         public string ProfessorsEmail { get; set; }
+        [Required]
         public string ProfessorsTel { get; set; }
+        [Required]
         public string ProfessorsFacultys { get; set; }
+        [Required]
         public string ProfessorsBranch { get; set; }
+        [Required]
         public int PositionId { get; set; }
         public string ProfessorslineId { get; set; }
         public string ProfessorsFacebookId { get; set; }
